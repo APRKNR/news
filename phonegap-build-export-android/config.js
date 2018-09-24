61132b758f5044ad85fe69af7e6b0635dc0a0101
APRKNR <42615726+APRKNR@users.noreply.github.com>
2018-09-24 16:03:57 +0530

    Add files via upload

---
+++ phonegap-build-export-android/config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'android',
+		wp_ws_url : 'http://trueeyenews.com/wp-appkit-api/android',
+		wp_url : 'http://trueeyenews.com',
+		theme : 'q-android',
+		version : '',
+		app_type : 'phonegap-build',
+		app_title : '',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 5,
+		debug_mode : 'off',
+		auth_key : 'U5KwbA 3$<%k/ldn/P!+84H=|~9uZd,za4*NC6ymVx+lMe}ED6*;XCZEh%;nf@NU',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
